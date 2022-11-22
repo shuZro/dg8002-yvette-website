f3d710873383f6b9216c53722db891241bded4be
Shuprio Shourov <shuprio3@yahoo.com>
2022-11-21 21:12:46 -0500

    add dress images and update data

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,8 +1,74 @@
 data = `[
   {
     "image": "assets/images/dresses/01.png",
-    "title": "",
+    "title": "Dress 1",
     "price": 40,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/02.png",
+    "title": " Dress 2",
+    "price": 35,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/03.png",
+    "title": " Dress 4",
+    "price": 65,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/04.png",
+    "title": " Dress 5",
+    "price": 79,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/05.png",
+    "title": " Dress 6",
+    "price": 100,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/06.png",
+    "title": " Dress 7",
+    "price": 50,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/07.png",
+    "title": " Dress 8",
+    "price": 105,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/08.png",
+    "title": " Dress 9",
+    "price": 110,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/09.png",
+    "title": " Dress 10",
+    "price": 84,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/10.png",
+    "title": " Dress 11",
+    "price": 67,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/11.png",
+    "title": " Dress 12",
+    "price": 98,
+    "description": ""
+  },
+  {
+    "image": "assets/images/dresses/12.png",
+    "title": " Dress 6",
+    "price": 100,
     "description": ""
   }
 ]`;
