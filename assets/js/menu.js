3e31128125d612ea5df8deea0b34a632483f9f9c
Shuprio Shourov <shuprio3@yahoo.com>
2022-11-21 21:08:44 -0500

    refactor menu bar

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -8,18 +8,18 @@ html = '<nav class="navbar navbar-expand-md navbar-light bg-light">' +
                         '<input class="form-control" type="text" name="Search" placeholder="Search...">' +
                     '</div>' +
                 '</li>' +
-                '<li class="nav-item"><a class="nav-link" href="" data-target="#myModal" data-toggle="modal"><i class="material-icons">account_circle</i></a></li>' +
-                '<li class="nav-item"><a class="nav-link" href="" data-target="#myModal" data-toggle="modal"><i class="material-icons">favorite</i></a></li>' +
-                '<li class="nav-item"><a class="nav-link" href="checkout.html" data-target="#myModal" data-toggle="modal"><i class="material-icons">shopping_cart</i></a></li>' +
+                '<li class="nav-item"><a class="nav-link" href=""><i class="material-icons">account_circle</i></a></li>' +
+                '<li class="nav-item"><a class="nav-link" href=""><i class="material-icons">favorite</i></a></li>' +
+                '<li class="nav-item"><a class="nav-link" href="checkout.html"><i class="material-icons">shopping_cart</i></a></li>' +
             '</ul>' +
         '</nav>' +
         '<nav class="navbar navbar-expand-md navbar-light bg-light justify-content-center">' +
-            '<a class="navbar-brand" href="index.html">NEW</a>' +
-            '<a class="navbar-brand" href="index.html">BRIDAL DRESS</a>' +
-            '<a class="navbar-brand" href="index.html">PROM DRESS</a>' +
-            '<a class="navbar-brand" href="index.html">SALE</a>' +
-            '<a class="navbar-brand" href="index.html">RENTALS</a>' +
-            '<a class="navbar-brand" href="index.html">GET INSPIRED</a>' +
+            '<a class="navbar-brand" href="shop.html">NEW</a>' +
+            '<a class="navbar-brand" href="shop.html">BRIDAL DRESS</a>' +
+            '<a class="navbar-brand" href="shop.html">PROM DRESS</a>' +
+            '<a class="navbar-brand" href="shop.html">SALE</a>' +
+            '<a class="navbar-brand" href="shop.html">RENTALS</a>' +
+            '<a class="navbar-brand" href="shop.html">GET INSPIRED</a>' +
         '</nav>'
 
 document.querySelector("#menu").innerHTML = html;
