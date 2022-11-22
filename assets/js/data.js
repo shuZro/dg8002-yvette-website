16ac1e37d14cdf35f35354ee48151db344af73ca
Shuprio Shourov <shuprio3@yahoo.com>
2022-11-21 21:40:15 -0500

    add basic rating filtering

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,72 +3,96 @@ data = `[
     "image": "assets/images/dresses/01.png",
     "title": "Dress 1",
     "price": 40,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 4
   },
   {
     "image": "assets/images/dresses/02.png",
     "title": " Dress 2",
     "price": 35,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 1
   },
   {
     "image": "assets/images/dresses/03.png",
     "title": " Dress 4",
     "price": 65,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 4
   },
   {
     "image": "assets/images/dresses/04.png",
     "title": " Dress 5",
     "price": 79,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 3
   },
   {
     "image": "assets/images/dresses/05.png",
     "title": " Dress 6",
     "price": 100,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 3
   },
   {
     "image": "assets/images/dresses/06.png",
     "title": " Dress 7",
     "price": 50,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 2
   },
   {
     "image": "assets/images/dresses/07.png",
     "title": " Dress 8",
     "price": 105,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 4
   },
   {
     "image": "assets/images/dresses/08.png",
     "title": " Dress 9",
     "price": 110,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 5
   },
   {
     "image": "assets/images/dresses/09.png",
     "title": " Dress 10",
     "price": 84,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 1
   },
   {
     "image": "assets/images/dresses/10.png",
     "title": " Dress 11",
     "price": 67,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 4
   },
   {
     "image": "assets/images/dresses/11.png",
     "title": " Dress 12",
     "price": 98,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 2
   },
   {
     "image": "assets/images/dresses/12.png",
     "title": " Dress 6",
     "price": 100,
-    "description": ""
+    "description": "",
+    "colour": "white",
+    "rating": 3
   }
 ]`;
--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,15 +1,30 @@
 const shopData = JSON.parse(data);
+loadItems(shopData)
 
-let html = ''
-shopData.forEach(function myFunction(item) {
-    html += '<div class="p-2">' +
+function loadItems(shopData) {
+    let html = ''
+    shopData.forEach(function myFunction(item) {
+        html += '<div class="p-2">' +
             '<div class="card" style="width: 11rem;">' +
-                '<a href="item.html"><img class="card-img-top" src="' + item.image + '" alt="Card image cap"></a>' +
-                    '<div class="card-body">' +
-                       '<p class="card-text">' + item.title + ': <b>$' + item.price + '</b></p>' +
-                    '</div>' +
+            '<a href="item.html"><img class="card-img-top" src="' + item.image + '" alt="Card image cap"></a>' +
+            '<div class="card-body">' +
+            '<p class="card-text">' + item.title + ': <b>$' + item.price + '</b></p>' +
             '</div>' +
-        '</div>'
-})
+            '</div>' +
+            '</div>'
+    })
 
-document.querySelector("#items").innerHTML = html;
+    document.querySelector("#items").innerHTML = html;
+}
+
+function filter(self, attribute) {
+    const select = document.querySelector('#' + self.id)
+    const selectedValues = [].filter
+        .call(select.options, option => option.selected)
+        .map(option => +option.value);
+
+    let shopData = JSON.parse(data);
+    shopData = shopData.filter(item => selectedValues.includes(item[attribute]))
+
+    loadItems(shopData)
+}
