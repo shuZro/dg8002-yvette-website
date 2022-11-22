c6672933fe6fae328ff80c302f3d9edeb6195b4d
sshourov <shuprio3@yahoo.com>
2022-11-22 14:25:30 -0500

    refactor shop.js

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -4,14 +4,14 @@ loadItems(shopData)
 function loadItems(shopData) {
     let html = ''
     shopData.forEach(function myFunction(item) {
-        html += '<div class="p-2">' +
-            '<div class="card" style="width: 11rem;">' +
-            '<a href="item.html"><img class="card-img-top" src="' + item.image + '" alt="Card image cap"></a>' +
-            '<div class="card-body">' +
-            '<p class="card-text">' + item.title + ': <b>$' + item.price + '</b></p>' +
-            '</div>' +
-            '</div>' +
-            '</div>'
+        html += `<div class="p-2">
+                <div class="card" style="width: 11rem;">
+                    <a href="item.html"><img class="card-img-top" src="${item.image}" alt="Card image cap"></a>
+                    <div class="card-body">
+                        <p class="card-text">${item.title}: <b>$${item.price}</b></p>
+                    </div>
+                </div>
+            </div>`
     })
 
     document.querySelector("#items").innerHTML = html;
